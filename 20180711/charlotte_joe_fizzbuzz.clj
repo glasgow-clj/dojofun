2897ffcb91c9ffb6c5bef7e96fce88e57a7e2891
Joe Tague <750181+joetague@users.noreply.github.com>
2018-07-12 13:44:22 +0100

    Attempt at FizzBuzz kata

---
+++ 20180711/charlotte_joe_fizzbuzz.clj
@@ -0,0 +1,15 @@
+(ns dojofun.core
+  (:gen-class))
+
+  (defn- fizzbuzz [n]
+    (cond
+      (zero? n) (str n)
+      (and (= (mod n 3) 0) (= (mod n 5) 0)) "fizzbuzz"
+      (= (mod n 3) 0)  "fizz"
+      (= (mod n 5) 0) "buzz"
+      :else (str n)
+    ))
+
+(defn buzzer []
+    (map fizzbuzz (range 101))
+    )
